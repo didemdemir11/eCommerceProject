438f6fcafb1400f0bf1e31cd7d64ca6f36893110
didemdemir11 <kdidemdemir@gmail.com>
2024-10-15 23:15:14 +0300

    add shop pages to layout

--- e-commerce-app/src/pages/HomePage.jsx
+++ e-commerce-app/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Welcome to YourStoreName!</h1>
+      <h1 className="text-2xl font-bold">Welcome to Didi Store!</h1>
       <p>Discover amazing products at great prices.</p>
       {/* Add more content like featured products, banners, etc. */}
     </div>
