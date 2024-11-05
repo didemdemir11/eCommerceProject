c254ab89da4beee7fc94ca568fa9e4367765e5d8
didemdemir11 <kdidemdemir@gmail.com>
2024-11-05 22:05:54 +0300

    Install react-hook-form package and update userReducer

--- e-commerce-app/src/store/reducers/userReducer.js
+++ e-commerce-app/src/store/reducers/userReducer.js
@@ -2,12 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
   name: "user",
-  initialState: (userInfo: null),
+  initialState: { userInfo: null },
   reducers: {
     setUser: (state, action) => {
       state.userInfo = action.payload;
     },
   },
 });
+
 export const { setUser } = userSlice.actions;
 export default userSlice.reducer;
